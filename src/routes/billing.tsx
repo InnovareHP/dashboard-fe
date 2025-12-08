101042c77ffdfebe3a1024c969a8c2e43fa85f78
redcloud442 <markivor.glorioso@gmail.com>
2025-12-09 00:38:04 +0800

    add fix

--- src/routes/billing.tsx
+++ src/routes/billing.tsx
@@ -1,6 +1,6 @@
 import { BillingPage } from "@/components/billing-page";
 import { authClient } from "@/lib/auth-client";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import type { Session, User } from "better-auth";
 
 export const Route = createFileRoute("/billing")({
@@ -8,6 +8,11 @@ export const Route = createFileRoute("/billing")({
     const user = context.context.user as unknown as User & {
       user_is_onboarded: boolean;
     };
+
+    if (!user) {
+      throw redirect({ to: "/login" });
+    }
+
     const session = context.context.session as unknown as Session & {
       activeOrganizationId: string;
     };
